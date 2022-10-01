f4eb212fa11ecf5282ae5e0ab37d85a0cb92964c
Emmanuel Joseph <emmjstud@gmail.com>
2022-10-01 05:53:55 -0700

    argument count

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints program namr with new line
+ * @argc: Argument Counter
+ * @arcv: array of pointers supplied 
+ *
+ * Return: Always 0.
+ */
+int main(int (void) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+
+	return (0);
+}
